341e47a87c7684264f6ed0eb2a4c23fabf698451
claudinemahoro <claudinemah3@gmail.com>
2020-10-04 00:52:46 +0200

    add jquery to what we do section for hiding description

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $(".photo1, .description1").click(function(){
+    $(".photo1").toggle();
+    $(".description1").toggle();
+  });
+  $(".photo2, .description2").click(function(){
+      $(".photo2").toggle();
+      $(".description2").toggle();
+  });
+  $(".photo3, .description3").click(function(){
+      $(".photo3").toggle();
+      $(".description3").toggle();
+  });
+});
+
+// $(document).ready(function() {
+//   $(".design, .call").click(function() {
+//     $("#design").toggle();
+//     $(".call").toggle();
+//   });
+// });
+//
